651e0d902fd3b38ad3359e623df589cfdbe365c7
kiven517 <kiven517@126.com>
2018-01-18 19:47:55 +0800

    测试使用默认环境

--- manage.py
+++ manage.py
@@ -14,8 +14,8 @@ def get_env_variable(var_name):
 
 
 if __name__ == "__main__":
-    DJANGO_EXECUTION_ENVIRONMENT = get_env_variable('DJANGO_EXECUTION_ENVIRONMENT')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.local")
+    DJANGO_EXECUTION_ENVIRONMENT = get_env_variable('DJANGO_EXECUTION_ENVIRONMENT')
     # if DJANGO_EXECUTION_ENVIRONMENT == 'LOCAL':
     #     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings.local")
     # if DJANGO_EXECUTION_ENVIRONMENT == "TEST":
